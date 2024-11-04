dd7844eaf90f66adaf2f5219e8e3a128db65c2d1
Xiaoyu <2794209812@qq.com>
2024-11-04 13:36:36 +0800

    fix: fix provider key

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ function announceProvider() {
 
     window.dispatchEvent(
         new CustomEvent('eip6963:announceProvider', {
-            detail: Object.freeze({ info, ethereumProvider }),
+            detail: Object.freeze({ info, provider: ethereumProvider }),
         }),
     );
 }
